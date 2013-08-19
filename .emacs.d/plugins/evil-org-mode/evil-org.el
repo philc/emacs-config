d4696f5a5ad80700193f79762860c9f1ab43d8d9
Phil Crosby <phil.crosby@gmail.com>
2013-08-18 18:38:40 -0700

    Add evil org mode

---
+++ .emacs.d/plugins/evil-org-mode/evil-org.el
@@ -0,0 +1,98 @@
+;;; evil-org-mode.el --- evil keybindings for org-mode
+;;; NOTE(philc): This adds the keybindings you would expect. I've tweaked a few of
+;;; them in-line.
+
+;; Copyright (C) 2012-2013 by Edward Tjörnhammar
+;; Author: Edward Tjörnhammar
+;; URL: https://github.com/edwtjo/evil-org-mode.git
+;; Git-Repository; git://github.com/edwtjo/evil-org-mode.git
+;; Created: 2012-06-14
+;; Version: 0.1.0
+;; Package-Requires: ((evil "0"))
+;; Keywords: evil vim-emulation org-mode key-bindings presets
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; Known Bugs:
+;; See, https://github.com/edwtjo/evil-org-mode/issues
+
+(require 'evil)
+(require 'org)
+
+(define-minor-mode evil-org-mode
+  "Buffer local minor mode for evil-org"
+  :init-value nil
+  :lighter " EvilOrg"
+  :keymap (make-sparse-keymap) ; defines evil-org-mode-map
+  :group 'evil-org)
+
+(add-hook 'org-mode-hook 'evil-org-mode) ;; only load with org-mode
+
+(defun always-insert-item ()
+  (if (not (org-in-item-p))
+      ; (insert "\n- ")
+      (insert "\n")
+    (org-insert-item))
+  )
+
+(defun evil-org-eol-call (fun)
+  (end-of-line)
+  (funcall fun)
+  (evil-append nil)
+  )
+
+;; normal state shortcuts
+(evil-define-key 'normal evil-org-mode-map
+  "gh" 'outline-up-heading
+  "gj" 'org-forward-same-level
+  "gk" 'org-backward-same-level
+  "gl" 'outline-next-visible-heading
+  "t" 'org-todo
+  "T" '(lambda () (interactive) (evil-org-eol-call '(org-insert-todo-heading nil)))
+  "H" 'org-beginning-of-line
+  "L" 'org-end-of-line
+  ";vt" 'org-show-todo-tree
+  "o" '(lambda () (interactive) (evil-org-eol-call 'always-insert-item))
+  ; "O" '(lambda () (interactive) (evil-org-eol-call 'org-insert-heading))
+  "$" 'org-end-of-line
+  "^" 'org-beginning-of-line
+  "<" 'org-metaleft
+  ">" 'org-metaright
+  ";a" 'org-archive-subtree
+  ";va" 'org-agenda
+  "-" 'org-cycle-list-bullet
+  (kbd "TAB") 'org-cycle)
+
+;; normal & insert state shortcuts.
+(mapc (lambda (state)
+        (evil-define-key state evil-org-mode-map
+          (kbd "M-l") 'org-metaright
+          (kbd "M-h") 'org-metaleft
+          (kbd "M-k") 'org-metaup
+          (kbd "M-j") 'org-metadown
+          (kbd "M-L") 'org-shiftmetaright
+          (kbd "M-H") 'org-shiftmetaleft
+          (kbd "M-K") 'org-shiftmetaup
+          (kbd "M-J") 'org-shiftmetadown
+    ; M-return creates a new todo item and enters insert mode.
+          (kbd "<M-return>") '(lambda () (interactive)
+                                (org-insert-heading-after-current)
+                                (evil-append nil))
+          ))
+      '(normal insert))
+
+(provide 'evil-org)
+;;; evil-org.el ends here
