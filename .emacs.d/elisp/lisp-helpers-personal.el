0063d9bd1bf0ad72931512fece5af4daf98ef985
Phil Crosby <phil.crosby@gmail.com>
2014-02-26 18:23:09 -0800

    Add a define-keys function, which is like define-key but is variadic

--- .emacs.d/elisp/lisp-helpers-personal.el
+++ .emacs.d/elisp/lisp-helpers-personal.el
@@ -84,3 +84,14 @@
 (defun without-confirmation (fn)
   (flet ((y-or-n-p (&rest args) t)) ; Skip the confirmation prompts.
     (funcall fn)))
+
+;; Taken from https://groups.google.com/forum/#!topic/gnu.emacs.help/_p2-GXAANgw. Requires CL.
+(defun partition (l n)
+  "Return a list of L's consecutive sublists of length N."
+  (assert (zerop (mod (length l) n)))
+  (loop for l on l by #'(lambda (l) (nthcdr n l)) collect (subseq l 0 n)))
+
+(defun define-keys (keymap &rest key-and-fn-pairs)
+  "Like define-key, but takes a variable number of arguments -- two per key binding pair."
+  (dolist (pair (partition key-and-fn-pairs 2))
+    (define-key keymap (first pair) (second pair))))
