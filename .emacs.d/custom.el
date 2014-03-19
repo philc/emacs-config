35e8900281cf4a4bf2e2776f074cc5d44c54eacf
Phil Crosby <phil.crosby@gmail.com>
2014-03-18 23:03:12 -0700

    Make the cited font faces non-italic in mu4e

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -17,9 +17,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elscreen-tab-control-face ((t (:background "#202424" :foreground "white" :underline t))))
- '(elscreen-tab-current-screen-face ((t (:background "#505454" :foreground "white"))))
- '(elscreen-tab-other-screen-face ((t (:background "black" :foreground "#bbbbbb" :underline t))))
+ '(elscreen-tab-control-face ((t (:background "#202424" :foreground "white" :underline t))) t)
+ '(elscreen-tab-current-screen-face ((t (:background "#505454" :foreground "white"))) t)
+ '(elscreen-tab-other-screen-face ((t (:background "black" :foreground "#bbbbbb" :underline t))) t)
+ '(mu4e-cited-1-face ((t (:inherit font-lock-builtin-face :weight normal))))
+ '(mu4e-cited-2-face ((t (:inherit font-lock-type-face :weight normal))))
+ '(mu4e-cited-3-face ((t (:inherit font-lock-variable-name-face :weight normal))))
+ '(mu4e-cited-4-face ((t (:inherit font-lock-keyword-face :weight normal))))
+ '(mu4e-cited-5-face ((t (:inherit font-lock-comment-face :weight normal))))
+ '(mu4e-cited-6-face ((t (:inherit font-lock-comment-delimiter-face :weight normal))))
+ '(mu4e-cited-7-face ((t (:inherit font-lock-preprocessor-face :weight normal))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#3A8CD7"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#44D7BC"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "#C9D736"))))
