43430e4d124e6fd6e4e40d8eb7d3bbbca734c839
Phil Crosby <phil.crosby@gmail.com>
2014-07-03 17:00:46 -0700

    Slight refactor & cleanup

--- .emacs.d/elisp/cider-test.el
+++ .emacs.d/elisp/cider-test.el
@@ -7,15 +7,18 @@
 ;;
 (provide 'cider-test)
 (require 'cider)
+(require 'cl)
 
-(defun cider-test/run-tests-in-ns ()
-  "Saves and evals the buffer, and then runs any clojure.test tests defined in the current namespace."
-  (interactive)
+(defun when-compile-successful (f)
+  "Saves and loads the current buffer, and compiles it. Runs the given function if there are no compile
+  errors."
   (save-buffer)
   (cider-load-current-buffer)
-  (sleep-for 0.1) ; cider-load-current-buffer is asynchronous, unfortunately.
+  (sleep-for 0.1) ; cider-load-current-buffer is asynchronous, so we must work around that.
+  ;; If there was a compile error, halt. Otherwise we'll mask the compile error which has been printed to the
+  ;; minibuffer.
   (when (not (cider-test/buffer-has-compile-errors?))
-    (cider-interactive-eval "(clojure.test/run-tests)")))
+    (funcall f)))
 
 (defun cider-test/defun-name-at-point ()
   "Returns the name of the function at point, and nil if it can't be found."
@@ -34,6 +37,26 @@
          (cider-notes (remove-if-not (lambda (o) (overlay-get o 'cider-note-p)) overlays)))
     (> (length cider-notes) 0)))
 
+;; TODO(philc): This command doesn't work; we must first load the tests before running them.
+(defun cider-test/run-all-tests ()
+  "Saves and evals the buffer, and then runs any clojure.test tests defined in the p;roject."
+  (interactive)
+  (let ((project-dir (nrepl-project-directory-for (nrepl-current-dir))))
+    (print project-dir)
+    ;; TODO(philc): Make this a recursive file search.
+    (dolist (test-file (or argv (directory-files project-dir t "_test.clj$")))
+      (print test-file)
+      (cider-load-file-op test-file nil t)))
+  (when-compile-successful
+   (lambda () (cider-interactive-eval "(clojure.test/run-all-tests)"))))
+
+(defun cider-test/run-tests-in-ns ()
+  "Saves and evals the buffer, and then runs any clojure.test tests defined in the current namespace."
+  (interactive)
+  (save-buffer)
+  (when-compile-successful
+   (lambda () (cider-interactive-eval "(clojure.test/run-tests)"))))
+
 (defun cider-test/run-test-at-point ()
   "Runs the clojure.test under the cursor by invoking the function defined by the test in the cider repl."
   (interactive)
@@ -41,16 +64,11 @@
   ;; our last eval. We use cider-load-current-buffer instead of cider-eval-defun-at-point for this because
   ;; load-current-buffer properly sets the file metadata for the function definition, so that test failure
   ;; output has the correct source file and line number of the failing test.
-  (save-buffer)
-  (cider-load-current-buffer)
-  (sleep-for 0.1) ; cider-load-current-buffer is asynchronous, unfortunately.
-  ;; If there was a compile error, halt. Otherwise we'll mask the compile error which has been printed to the
-  ;; minibuffer.
-  (if (cider-test/buffer-has-compile-errors?)
-      nil
-    (let ((fn-name (cider-test/defun-name-at-point)))
-      ;; TODO(philc): It would be nice if we showed whether the test passed or failed in the minibuffer.
-      ;; Currently we just show "nil", and one must look to the repl to see the test's output in the case of
-      ;; failure.
-      (when fn-name
-        (cider-interactive-eval (concat "(" fn-name ")"))))))
+  (when-compile-successful
+   (lambda ()
+     (let ((fn-name (cider-test/defun-name-at-point)))
+       ;; TODO(philc): It would be nice if we showed whether the test passed or failed in the minibuffer.
+       ;; Currently we just show "nil", and one must look to the repl to see the test's output in the case of
+       ;; failure.
+       (when fn-name
+         (cider-interactive-eval (concat "(" fn-name ")")))))))
