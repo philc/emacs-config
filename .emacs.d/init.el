58d68b98fb36e64f9bcf501e8328a767d4f35e9a
Phil Crosby <phil.crosby@gmail.com>
2015-01-01 19:55:14 -0800

    When jumping to a project, if it's clojure, open the handler.clj or core.clj immediately
    I almost always do this, to start a repl and get my bearings.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -311,7 +311,7 @@
   ;; "v" is a mnemonic prefix for "view X".
   ;; "vv" will be a natural choice as a mode-specific shortcut for previewing the current file.
   "vu" 'mu4e
-  "vp" 'open-root-of-project-in-dired
+  "vp" 'navigate-to-project
   "vn" 'open-markdown-file-from-notes-folder
   "vo" (lambda () (interactive) (find-file "~/Dropbox/tasks.org"))
   "ve" (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
@@ -1177,27 +1177,44 @@
            first
            find-file))))
 
-(defun open-root-of-project-in-dired ()
+(defun open-root-of-project (project-path)
+  "Opens the project at path. If it's a clojure project, find the project's 'main' file and open that.
+   Otherwise, used dired to open the file in `path`."
+  ;; NOTE(philc): This function opens the "main" files in the project types that I typically work on.
+  ;; Customzie this to meet your needs if you want this functionality.
+  (let* ((project-name (file-name-nondirectory project-path))
+         (is-clojure (file-exists-p (concat path "/project.clj")))
+         (main-file (when is-clojure
+                      (->> ["core.clj" "handler.clj"]
+                           (mapcar (lambda (file) (concat path "/src/" project-name "/" file)))
+                           (remove-if-not 'file-exists-p)
+                           first))))
+    (if main-file
+        (set-window-buffer (selected-window) (find-file main-file))
+      (dired project-path))))
+
+(defun navigate-to-project ()
   "Prompts for the name of a project which exists in your common project folders and opens a dired window in
    the root of the project folder. This is a fast way to open a new project and be able to run
    projectile-file-file.
    Once a project is chosen, the current elscreen-tab is set to be the name of that project."
   (interactive)
-  (let ((all-project-folders (->> project-folders
-                                  (mapcar (lambda (file)
-                                            (filter-files-in-directory file 'file-directory-p nil)))
-                                  flatten)))
-    (let ((project-to-open (ido-completing-read "Project folder: "
-                                                (mapcar 'file-name-nondirectory all-project-folders)
-                                                nil t)))
-      (->> all-project-folders
-           (remove-if-not (lambda (project) (string/ends-with project (concat "/" project-to-open))))
-           first
-           ((lambda (project)
-              (dired project)
-              ;; If we invoke this inside of a split, don't set the tab's title.
-              (when (= 1 (length (window-list)))
-                (escreen-set-tab-alias (file-name-nondirectory project)))))))))
+  (let* ((all-project-folders (->> project-folders
+                                   (mapcar (lambda (file)
+                                             (filter-files-in-directory file 'file-directory-p nil)))
+                                   flatten))
+         (project-to-open (ido-completing-read "Project folder: "
+                                               (mapcar 'file-name-nondirectory all-project-folders)
+                                               nil t))
+         (project (->> all-project-folders
+                       (remove-if-not (lambda (project)
+                                        (string/ends-with project (concat "/" project-to-open))))
+                       first)))
+    (open-root-of-project project)
+    ;; If we invoke this inside of a split, don't set the tab's title.
+    (when (= 1 (length (window-list)))
+      (escreen-set-tab-alias (file-name-nondirectory project)))))
+
 
 ;;
 ;; JSON
