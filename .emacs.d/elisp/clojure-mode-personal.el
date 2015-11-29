df724f4922600b070184c46e3044eff269cb8267
Phil Crosby <phil.crosby@gmail.com>
2015-11-28 18:58:16 -0800

    Clojure: shortcuts for moving between s-exps

--- .emacs.d/elisp/clojure-mode-personal.el
+++ .emacs.d/elisp/clojure-mode-personal.el
@@ -24,6 +24,8 @@
 
 (evil-define-key 'normal clojure-mode-map "gf" 'cider-jump-to-var)
 (evil-define-key 'normal clojure-mode-map "gb" 'cider-jump-back)
+(evil-define-key 'normal clojure-mode-map "(" 'sp-backward-up-sexp)
+(evil-define-key 'normal clojure-mode-map ")" 'sp-forward-sexp)
 (evil-define-key 'normal clojure-mode-map (kbd "C-S-H") 'shift-sexp-backward)
 (evil-define-key 'normal clojure-mode-map (kbd "C-S-L") 'shift-sexp-forward)
 (evil-define-key 'normal clojure-mode-map (kbd "C-S-K") 'shift-sexp-backward)
