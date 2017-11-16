7a7cd317b4fb1da3dc328ef7e4a6fbd3a7075ea6
Phil Crosby <phil.crosby@gmail.com>
2017-11-15 19:40:11 -0800

    Generated changes to custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,7 +7,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cider-repl-pop-to-buffer-on-connect nil)
- '(org-agenda-files (quote ("~/test.org"))))
+ '(org-agenda-files (quote ("~/test.org")))
+ '(package-selected-packages
+   (quote
+    (browse-at-remote parinfer zoom-frm yasnippet yaml-mode web-mode wcheck-mode tree-mode swift-mode spinner smex smartparens slamhound seq scss-mode ruby-electric rainbow-delimiters queue projectile powerline outline-magic org notmuch mustache-mode multi-term mu4e-maildirs-extension maxframe markdown-mode magit lua-mode less-css-mode inf-clojure ido-vertical-mode ido-ubiquitous ht hiwin gradle-mode go-mode framemove flx-ido fiplr fill-column-indicator f eyebrowse evil-surround evil-nerd-commenter evil-leader evil-anzu escreen edit-server dired-details+ dired+ diminish dash-functional dash-at-point column-marker color-theme-solarized color-theme-sanityinc-tomorrow coffee-mode autopair auto-complete align-cljlet ag ace-jump-mode))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -15,6 +18,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cider-error-highlight-face ((t (:inherit nil :underline "brown4"))))
+ '(markdown-list-item1-face ((t (:foreground "#ECE27C"))))
+ '(markdown-list-item2-face ((t (:foreground "#85C7FF"))))
+ '(markdown-list-item3-face ((t (:foreground "#E4BDA7"))))
+ '(markdown-list-item4-face ((t (:foreground "#87D952"))))
+ '(markdown-list-item5-face ((t (:foreground "#C3D9C9"))))
  '(mu4e-cited-1-face ((t (:inherit font-lock-builtin-face :weight normal))))
  '(mu4e-cited-2-face ((t (:inherit font-lock-type-face :weight normal))))
  '(mu4e-cited-3-face ((t (:inherit font-lock-variable-name-face :weight normal))))
@@ -22,12 +30,6 @@
  '(mu4e-cited-5-face ((t (:inherit font-lock-comment-face :weight normal))))
  '(mu4e-cited-6-face ((t (:inherit font-lock-comment-delimiter-face :weight normal))))
  '(mu4e-cited-7-face ((t (:inherit font-lock-preprocessor-face :weight normal))))
- '(markdown-list-item1-face ((t (:foreground "#ECE27C"))))
- '(markdown-list-item2-face ((t (:foreground "#85C7FF"))))
- '(markdown-list-item3-face ((t (:foreground "#E4BDA7"))))
- '(markdown-list-item4-face ((t (:foreground "#87D952"))))
- '(markdown-list-item5-face ((t (:foreground "#C3D9C9"))))
-
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#3A8CD7"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#44D7BC"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "#C9D736"))))
