5aabcfad30f2cbd98786a04f02283fa05e8e1fe6
Phil Crosby <phil.crosby@gmail.com>
2017-11-17 14:00:13 -0800

    Rename one of my categories

--- .emacs.d/elisp/org-mode-personal.el
+++ .emacs.d/elisp/org-mode-personal.el
@@ -165,10 +165,10 @@
   "Prompts for a TODO and the name of a top-level heading, and adds the TODO as a child to the heading."
   (interactive)
   ;; NOTE(philc): These are personalized to the way I organize my org mode TODO file.
-  (message "[L] Liftoff  [H] Home  [S] Study  [N] Entertainment  [M] Emacs [J] Journal")
+  (message "[L] Liftoff  [B] Base  [S] Study  [N] Entertainment  [M] Emacs [J] Journal")
   (lexical-let ((subheading (pcase (read-char)
                               (?l "Liftoff")
-                              (?h "Home")
+                              (?b "Base")
                               (?s "Study")
                               (?j "Journal")
                               (?n "Entertainment")
@@ -186,7 +186,7 @@
   (interactive)
   "Prompts for the name of a top-level heading and jumps to there."
   ;; TODO(philc): Populate these completions with the top-level headers from the buffer.
-  (let* ((headings '("Liftoff" "Home" "Study" "Entertainment" "Emacs" "Journal" "Gumshoe" "Side projects"
+  (let* ((headings '("Liftoff" "Base" "Study" "Entertainment" "Emacs" "Journal" "Gumshoe" "Side projects"
                      "Vimium" "Email"))
          (heading (ido-completing-read "Heading: " headings)))
     (goto-char 0)
