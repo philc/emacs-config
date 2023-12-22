cd02c46bc6896015a0d9359fe029f86825975188
Phil Crosby <phil.crosby@gmail.com>
2023-12-21 22:31:10 -0800

    Markdown: add a command to jump to a heading in the current file

--- .emacs.d/elisp/markdown-lite-mode.el
+++ .emacs.d/elisp/markdown-lite-mode.el
@@ -303,6 +303,7 @@
     (kbd "C-S-H") 'mlm/markdown-promote
     (kbd "C-S-A-L") 'mlm/markdown-demote-subtree
     (kbd "C-S-A-H") 'mlm/markdown-promote-subtree
+    "gh" 'mlm/navigate-to-heading
     "gu" 'mlm/markdown-up-heading)
 
   (evil-define-key 'insert markdown-lite-mode-map
@@ -1270,3 +1271,48 @@ Return nil if the current line is not the beginning of a list item."
     ;;        markdown-mode-font-lock-keywords-core))
     (setq font-lock-defaults '(mlm/markdown-mode-font-lock-keywords-basic))
     (font-lock-refresh-defaults)))
+
+(defvar mlm/heading-prefix "* ")
+(defvar mlm/heading-regexp "^\\* ")
+
+(defun mlm/get-top-level-headings ()
+  "Return a list of all top-level headings in the current buffer."
+  (interactive)
+  (let ((headings '()))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward mlm/heading-regexp nil t)
+        (let ((heading-text (buffer-substring-no-properties
+                             (+ (line-beginning-position) (length mlm/heading-prefix))
+                             (line-end-position))))
+          (push heading-text headings))))
+    (nreverse headings)))
+
+(defun mlm/goto-heading (heading-text)
+  "Navigates to the given heading, if it exists. `heading` should exclude the list item
+   character."
+  (let ((heading-line-number nil))
+    (save-excursion
+      (goto-char (point-min))
+      (while (and (null heading-line-number)
+                  (re-search-forward mlm/heading-regexp nil t))
+        (let ((text (buffer-substring-no-properties
+                     (+ (line-beginning-position) (length mlm/heading-prefix))
+                     (line-end-position))))
+          (when (string= heading-text text)
+            (setq heading-line-number (line-number-at-pos))))))
+    (when heading-line-number
+      (goto-line heading-line-number)
+      (recenter))))
+
+(defun mlm/navigate-to-heading ()
+  "Show a menu of headings and jump to the one selected."
+  (interactive)
+  (let* ((headings (mlm/get-top-level-headings))
+         (selected (ido-completing-read "Heading: " headings)))
+    (mlm/goto-heading selected)))
+
+
+(defun mlm/testing ()
+  (interactive)
+  (mlm/navigate-to-heading "Cursor position"))
