philc/emacs-config

commit ef7d34871d7459cb74485863b761a516219688e4

Change order of statements

.emacs.d/init.el
@@ -832,13 +832,13 @@
 ;; Dired mode - using the Emacs file browser.
 ;;
 
-(with-eval-after-load 'dired (configure-dired-mode))
-
 (defun configure-dired-mode ()
   ;; For some reason, dired's keymap overrides the keys of Evil mode's leader keys, and my global
   ;; macos-keys-minor-mode. To avoid fighting with it, I'm clearing the default dired keymap,
   ;; and installing only the keybindings that I use.
   (setq dired-mode-map (make-sparse-keymap))
+
+(with-eval-after-load 'dired (configure-dired-mode))
 
   (evil-define-key 'normal dired-mode-map
     (kbd "gu") (lambda () (interactive) (find-alternate-file ".."))

commit 436ac1cd8f36b5ece8f2a761b15b335c767b3c85

Fix dired mode keybindings
I broke this in ef7d34871d7459cb74485863b761a516219688e4.

.emacs.d/init.el
@@ -838,8 +838,6 @@
   ;; and installing only the keybindings that I use.
   (setq dired-mode-map (make-sparse-keymap))
 
-(with-eval-after-load 'dired (configure-dired-mode))
-
   (evil-define-key 'normal dired-mode-map
     (kbd "gu") (lambda () (interactive) (find-alternate-file ".."))
     "H" (lambda () (interactive) (find-alternate-file ".."))
@@ -860,6 +858,8 @@
     ;; The "e" prefix is for execute.
     "ed" 'dired-do-flagged-delete
     "em" 'dired-do-rename))
+
+(with-eval-after-load 'dired (configure-dired-mode))
 
 ;; By default dired mode shows the file's permissions, access time, size, etc. I just want to see
 ;; the file names.